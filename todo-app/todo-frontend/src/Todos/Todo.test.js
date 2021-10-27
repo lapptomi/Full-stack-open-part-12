0e70754ec511a37293d1c12135ec1fb26e830059
lapptomi <lappetelainen.tomi@gmail.com>
2021-10-27 13:50:37 +0300

    Part 12 - Exercise 12.14 done

---
+++ todo-app/todo-frontend/src/Todos/Todo.test.js
@@ -0,0 +1,22 @@
+import Todo from "./Todo"
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+
+test('renders content', () => {
+  const todo = {
+    text: 'Random-todo-component',
+    done: true
+  }
+
+  const component = render(
+    <Todo
+      className='todo'
+      key={todo} 
+      todo={todo}
+      onClickDelete={() => console.log('Delete clicked')}
+      onClickComplete={() => console.log('Complete clicked')}
+    />
+  )
+
+  expect(component.container).toHaveTextContent(todo.text)
+})
